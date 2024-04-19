04bfd23e67d17c7ffb9089ce4d467a233308f3b2
egaunnac <egaunnac@gmail.com>
2024-04-18 20:48:35 -0600

    garnet: Setup sim restart shell script
    sm8450 and above have a very annoying SMS receiving bug where for some carriers it won't work
    after a device restart if QTI telephony injection isn't available, but one of the workarounds
    is to either use airplane mode toggle or eject the sim card and reinsert it
    
    Instead of doing that? Let's try and automate disabling and enabling the sim card
    
    Co-authored-by: Marc Bourgoin <themard85@gmail.com>
    Change-Id: I76732c2a668ff179e32050aee4abe315de3d9357

--- device.mk
+++ device.mk
@@ -278,6 +278,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.garnet.rc \
     init.qcom.rc \
+    init.sim.restart.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd-odm.rc \
