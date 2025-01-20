1be9949d67b0dd350f7feeb9e9417e6550eb9025
bezke <bezke1@gmail.com>
2025-01-20 17:53:55 +0100

    garnet: Add support for audio HAL v8.0 implementation
    * Added `android.hardware.audio@8.0-impl` to PRODUCT_PACKAGES
    * Included `android.hardware.audio.effect@8.0-impl` for audio effects
    * Integrated `android.hardware.audio.service` to enhance audio service functionality
    
    This change updates the build configuration to include the Audio HAL (Hardware Abstraction Layer) v8.0 and its associated components, ensuring compatibility with the latest audio framework updates.

--- device.mk
+++ device.mk
@@ -44,8 +44,8 @@ SOONG_CONFIG_android_hardware_audio += \
 SOONG_CONFIG_android_hardware_audio_run_64bit := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@8.0-impl \
+    android.hardware.audio.effect@8.0-impl \
     android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
