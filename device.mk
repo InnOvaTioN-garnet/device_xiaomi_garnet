7684d09ea083d0b6e50203241abc265df37094a2
bezke <bezke1@gmail.com>
2025-01-20 18:06:34 +0100

    garnet: Add updated OpenGL ES and Vulkan DEQP level permissions
    * Added `android.software.opengles.deqp.level-2023-03-01.xml` to vendor permissions
    * Added `android.software.vulkan.deqp.level-2023-03-01.xml` to vendor permissions
    
    This change updates the PRODUCT_COPY_FILES to include the latest DEQP (DrawElements Quality Program) level configuration files for OpenGL ES and Vulkan. These files ensure the platform meets the required graphics compatibility standards.

--- device.mk
+++ device.mk
@@ -234,8 +234,8 @@ PRODUCT_PACKAGES += \
     libgui_vendor
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
-    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
